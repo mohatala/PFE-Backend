7c7aafd0584433d221ecf6ca2f99ad65a3829803
Talaini <talainimohammed@gmail.com>
2023-05-31 15:02:06 +0100

    updates

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const ejs = require('ejs');
 const lodash = require('lodash');
 const mongoose = require('mongoose');
 const app=express();
-
+const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -52,6 +52,6 @@ app.get('/posts/:postid',function (req,res) {
   });
 
 });
-app.listen(process.env.PORT, 'localhost', () => {
-  console.log(`Server running `);
+app.listen(PORT, 'localhost', () => {
+  console.log(`Server running ${PORT}`);
 });
